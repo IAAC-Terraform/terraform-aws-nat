cf92f9bdcf5a90183f13ebfe497d66e44c280329
Raghavan R <raghavan.r@capgemini.com>
2018-08-20 13:59:25 +1000

    Adding NAT gateway components

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "aws_nat_gateway" "ngw" {
+  count         = "${var.create_vpc && var.nat_gateway_route ? var.count : 0}"
+  allocation_id = "${var.allocation_id[count.index]}"
+  subnet_id     = "${var.subnet_id}"          # Public subnet id
+
+  tags {
+    Name = "${var.env}-ngw"
+  }
+}
+
