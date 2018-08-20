ae089d0dedfe0fef3e8c4a32522eab31a5a4a934
Raghavan R <raghavan.r@capgemini.com>
2018-08-20 14:25:35 +1000

    variable fix

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_nat_gateway" "ngw" {
   count         = "${var.create_vpc && var.nat_gateway_route ? var.count : 0}"
-  allocation_id = "${var.allocation_id[count.index]}"
+  allocation_id = "${element(var.allocation_id, count.index)}"
   subnet_id     = "${var.subnet_id}"          # Public subnet id
 
   tags {
